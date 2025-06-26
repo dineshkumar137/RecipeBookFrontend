2da312237c6dfd0bbb10ead85e790f1042f3bed9
Dinesh Kumar L <dineshkumar.l2022ece@sece.ac.in>
2025-06-26 05:37:13 +0530

    Final Changes

--- src/Components/Myrecipe.jsx
+++ src/Components/Myrecipe.jsx
@@ -14,7 +14,7 @@ export default function Myrecipe() {
   useEffect(() => {
   if (!user || !user.email) return;
 
-  fetch(`https://recipebookbackend-nexv.onrender.com/api//api/recipes/byemail/${user.email}`)
+  fetch(`https://recipebookbackend-nexv.onrender.com/api/recipes/byemail/${user.email}`)
     .then((res) => res.json())
     .then((data) => {
       console.log("Fetched recipes:", data);
